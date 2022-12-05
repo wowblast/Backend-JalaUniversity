11b3841976626ff08f12bddbe2120eee1bd4ef06
wowblast <jfrt142012@hotmail.com>
2022-12-05 15:29:27 -0400

    Add inversify

---
+++ homework05-12-2022inversify/src/app.ts
@@ -0,0 +1,7 @@
+import 'reflect-metadata'
+import { Service } from './service'
+import DIContainer  from './di-container'
+
+const service: Service = DIContainer.resolve<Service>(Service);
+
+console.log(service.getAllNames())
