5454daf3a452d81f23e958f57d31c9749987670d
wowblast <jfrt142012@hotmail.com>
2023-02-11 21:45:47 -0400

    fias

--- uploader-downloader-project/uploader/src/api/Infrastructure/googledrive/googledriveManager.ts
+++ uploader-downloader-project/uploader/src/api/Infrastructure/googledrive/googledriveManager.ts
@@ -1,15 +1,15 @@
 import { google } from "googleapis";
 export class googleDriveManager {
-    private CLIENT_ID= '282679733621-ijjt2he0fk861p8dq6p4c4glf1be1nlk.apps.googleusercontent.com'
-    private CLIENT_SECRET = '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';
+    private CLIENT_ID= ''
+    private CLIENT_SECRET = '';
     private REDIRECT_URI= 'https://developers.google.com/oauthplayground';
-    private refreshTRoken= '1//04jHW2bT2vFI7CgYIARAAGAQSNwF-L9IrBuBW4z4z4O066unJ5FWaL74ELpAT-j9pNJl7WHqRbmkb4SG4w7IUbHOKa1_i6V-qXRk'
+    private refreshTRoken= ''
 
     constructor() {
 
     }
 
-    uploadFileToGoogleDrive() {
+    uploadFileToGoogleDrive(fiel) {
         const authGoogle = new google.auth.OAuth2(
             this.CLIENT_ID,
             this.CLIENT_SECRET,
@@ -18,6 +18,10 @@ export class googleDriveManager {
         )
 
         authGoogle.setCredentials({refresh_token: this.refreshTRoken})
+        const drive = google.drive({
+            version:'v3',
+            auth:authGoogle
+        })
     }
     
 }
